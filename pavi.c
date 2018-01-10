04888e2286d335f8da0e36882c296e982624fc8e
nivetha80 <31503901+nivetha80@users.noreply.github.com>
2018-01-10 16:10:26 +0530

    Create pavi.c

---
+++ pavi.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("enter any integer");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("given no is even");
+}
+else
+{
+printf("given no is odd");
+}
+return 0;
+}
